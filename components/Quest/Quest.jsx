943938cd9e72d6d9fe1ec25252abbeac52727b8d
Robertas Ankudovicius <robertas.ankudovicius@twoday.com>
2024-09-04 19:30:42 +0300

    Refactored code

--- components/Quest/Quest.jsx
+++ components/Quest/Quest.jsx
@@ -1,12 +1,18 @@
+import { useEffect } from "react";
 import styles from "./styles.module.css";
-const Quest = ({ id, title, reward, setToken, setQuests, quests }) => {
+const Quest = ({ id, title, reward, onClick }) => {
+  useEffect(() => {
+    console.log(title);
+
+    return () => {
+      console.log(title + " DONE");
+    };
+  }, []);
+
   return (
     <div
       onClick={() => {
-        setToken((prevState) => prevState + reward);
-        const index = quests.findIndex((q) => q.id === id);
-        quests[index].isCompleted = true;
-        setQuests([...quests]);
+        onClick(id, reward);
       }}
       className={styles.main}
     >
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Avatar from "@/components/Avatar/Avatar";
 import { quests as questList } from "../data/quests";
@@ -13,6 +13,8 @@ const Main = () => {
   const [jump, setJump] = useState(1);
   const [fly, setFly] = useState(1);
 
+  const [isError, setError] = useState(false);
+
   const displayQuests = quests
     .filter((quest) => quest.isCompleted === false)
     .slice(0, 5);
@@ -20,6 +22,7 @@ const Main = () => {
   const buyPower = (powerTitle, sum) => {
     if (token < sum) {
       console.log("Not enough coins");
+      setError(true);
       return;
     }
 
@@ -38,55 +41,86 @@ const Main = () => {
     }
   };
 
+  const validateError = () => {
+    if (token >= speed * 100 || token >= jump * 150 || token >= fly * 200) {
+      setError(false);
+    }
+  };
+
+  const onClickQuest = (id, reward) => {
+    const index = quests.findIndex((q) => q.id === id);
+    quests[index].isCompleted = true;
+    setQuests([...quests]);
+
+    setToken((prevState) => prevState + reward);
+  };
+
+  // useEffect(() => {
+  //   console.log("some job");
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log("Speed value changed or initiolized");
+  // }, [speed]);
+
+  // useEffect(() => {
+  //   console.log("Speed or Jump or Fly value changed or initiolized");
+  // }, [speed, jump, fly]);
+
+  useEffect(() => {
+    validateError();
+  }, [token]);
+
   return (
-    <div className={styles.main}>
-      <div className={styles.characterReward}>
-        <div>
-          <Avatar imgUrl="https://i.ytimg.com/vi/oiLMkf1COP0/maxresdefault.jpg" />
+    <>
+      <div className={styles.main}>
+        <div className={styles.characterReward}>
+          <div>
+            <Avatar imgUrl="https://i.ytimg.com/vi/oiLMkf1COP0/maxresdefault.jpg" />
 
-          <div className={styles.token}>{token} coins</div>
+            <div className={styles.token}>{token} coins</div>
 
-          <PowerWrapper speed={speed} jump={jump} fly={fly} />
-        </div>
-
-        <div className={styles.righSideWrapper}>
-          <div className={styles.questWrapper}>
-            {displayQuests.map((quest) => (
-              <Quest
-                id={quest.id}
-                title={quest.title}
-                reward={quest.reward}
-                key={quest.id}
-                setToken={setToken}
-                setQuests={setQuests}
-                quests={quests}
-              />
-            ))}
+            <PowerWrapper speed={speed} jump={jump} fly={fly} />
           </div>
 
-          <button
-            className={styles.buyBtn}
-            onClick={() => buyPower("SPEED", speed * 100)}
-          >
-            Increase speed - {speed * 100}
-          </button>
+          <div className={styles.righSideWrapper}>
+            <div className={styles.questWrapper}>
+              {displayQuests.map((quest) => (
+                <Quest
+                  id={quest.id}
+                  title={quest.title}
+                  reward={quest.reward}
+                  key={quest.id}
+                  onClick={(id, reward) => onClickQuest(id, reward)}
+                />
+              ))}
+            </div>
 
-          <button
-            className={styles.buyBtn}
-            onClick={() => buyPower("JUMP", jump * 150)}
-          >
-            Increase jump - {jump * 150}
-          </button>
+            <button
+              className={styles.buyBtn}
+              onClick={() => buyPower("SPEED", speed * 100)}
+            >
+              Increase speed - {speed * 100}
+            </button>
 
-          <button
-            className={styles.buyBtn}
-            onClick={() => buyPower("FLY", fly * 200)}
-          >
-            Increase fly - {fly * 200}
-          </button>
+            <button
+              className={styles.buyBtn}
+              onClick={() => buyPower("JUMP", jump * 150)}
+            >
+              Increase jump - {jump * 150}
+            </button>
+
+            <button
+              className={styles.buyBtn}
+              onClick={() => buyPower("FLY", fly * 200)}
+            >
+              Increase fly - {fly * 200}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {isError && <h3 style={{ color: "#96001e" }}>Not enough coins</h3>}
+    </>
   );
 };
 
